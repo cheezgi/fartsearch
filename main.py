7e24d9e65d62df37119a104b92c159746cd1ceb1
cheezgi <zack.hixon@hotmail.com>
2016-02-27 22:57:33 -0500

    add to repo

---
+++ main.py
@@ -0,0 +1,11 @@
+#search the web for farts
+import os
+import sys
+
+#each crawled website is a seperate folder
+def create_project_dir(directory):
+	if not os.path.exists(directory):
+		print('new directory for ' + directory)
+		os.makedirs(directory)
+
+
